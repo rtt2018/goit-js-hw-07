f6f6fd066478407452d9f24849881e94a6bf3aae
TarasRogiv <r.taras.t.1@gmail.com>
2025-01-07 00:30:39 +0200

    upd 1 tasc

--- js/task-1.js
+++ js/task-1.js
@@ -3,7 +3,7 @@ console.log("Number of categories: ", listItemInCategories.length);
 listItemInCategories.forEach((node) => {
   node.childNodes.forEach((childNode) => {
     if (childNode.nodeName === "H2") {
-      console.log("Categoty: ", childNode.textContent);
+      console.log("Category: ", childNode.textContent);
     }
     if (childNode.nodeName === "UL") {
       console.log("Elements: ", childNode.children.length);
