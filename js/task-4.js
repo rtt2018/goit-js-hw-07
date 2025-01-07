b9f6aa7327dffef1f1294b839b0d270c5985f1e6
TarasRogiv <r.taras.t.1@gmail.com>
2025-01-08 01:23:51 +0200

    4 tasc done

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,22 @@
+const formLogin = document.querySelector(".login-form");
 
+formLogin.addEventListener("submit", formSubmit);
+
+function formSubmit(event) {
+  event.preventDefault();
+  if (
+    event.target.elements.email.value === "" ||
+    event.target.elements.password.value === ""
+  ) {
+    alert("'All form fields must be filled in'");
+  }
+  const formData = {};
+
+  formData[event.target.elements.email.name] =
+    event.target.elements.email.value;
+  formData[event.target.elements.password.name] =
+    event.target.elements.password.value;
+  console.log(formData);
+
+  event.target.reset();
+}
